63e2e5c016c0e0c48af860c28ddb16dee16448a6
Rjong1 <156464723+Rjong1@users.noreply.github.com>
2024-11-21 15:36:45 -0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 # Authors: Ronald Jong,
-# Seneca ID: 033295148, 
+# Seneca ID: 033295148
 # This script monitors the /var/log/auth.log file for suspicious activity and sends alerts.
+
+# Function to send an email alert
+def send_email_alert(subject, message):
+    """
+    Sends an email alert with the given subject and message.
+    """
+...
+
+# Main function
+def main():
+    """
+    Main function to initiate monitoring of the auth.log file.
+    """
+    # Path to the auth.log file
+    auth_log_path = '/var/log/auth.log'
+...
